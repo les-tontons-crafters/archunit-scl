8c9a674223ce2d520ac88fbfc5b1768be4562db5
Yoan Thirion <yoan.thirion@gmail.com>
2022-10-28 13:35:02 +0200

    add java skeleton

---
+++ java/src/test/java/rules/ArchitectureTests.java
@@ -0,0 +1,27 @@
+package rules;
+
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.lang.ArchRule;
+
+import static com.tngtech.archunit.core.importer.ImportOption.DoNotIncludeTests;
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
+import static com.tngtech.archunit.library.dependencies.SlicesRuleDefinition.slices;
+
+// Check documentation here : https://www.archunit.org/userguide/html/000_Index.html
+@AnalyzeClasses(packages = "rules", importOptions = DoNotIncludeTests.class)
+public class ArchitectureTests {
+    @ArchTest
+    public static final ArchRule cycles = ensureNoCycle("sample");
+
+    @ArchTest
+    public static final ArchRule exceptions_resides_in_pkg_exception = classes().that()
+            .areAssignableTo(Exception.class)
+            .or().areAssignableTo(RuntimeException.class)
+            .should().resideInAnyPackage("..exception..", "..exceptions..");
+
+    public static ArchRule ensureNoCycle(String projectPackage) {
+        return slices().matching(projectPackage + ".(*)..").should()
+                .beFreeOfCycles();
+    }
+}
